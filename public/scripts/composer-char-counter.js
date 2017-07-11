63afba4dc445612dcda473ab2fb36cd3b3b00655
Tyler Krys <tylerkrys@gmail.com>
2017-07-11 22:10:40 +0000

    Adds logic so keyup and keydown both update the character count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,14 +3,24 @@ $(document).ready(function() {
   var textArea = $('.new-tweet').find('textarea');
   $(textArea).on('focus', function() {
     //console.log("Focus is on textArea");
+    // DRY out of the two functions below on refactor
     $(this).on('keydown', function() {
       currentCharCount = this.value.length;
-      //console.log("The character count is " + currentCharCount);
       var remainingCharCount = 140 - this.value.length;
-      //console.log("The remaining character count is " + remainingCharCount);
       var counter = $(this).closest('form').find('.counter');
       $(counter).text(remainingCharCount);
-      // If remainingCharCount falls below zero, add red text class to counter
+      if (remainingCharCount < 0) {
+        counter.addClass('negativeCharCount');
+      }
+      if (remainingCharCount >= 0) {
+        counter.removeClass('negativeCharCount');
+      }
+    });
+    $(this).on('keyup', function() {
+      currentCharCount = this.value.length;
+      var remainingCharCount = 140 - this.value.length;
+      var counter = $(this).closest('form').find('.counter');
+      $(counter).text(remainingCharCount);
       if (remainingCharCount < 0) {
         counter.addClass('negativeCharCount');
       }
