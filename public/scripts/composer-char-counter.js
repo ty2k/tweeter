591d7ae4f78c5fe691a04534a0338928e28e326a
Tyler Krys <tylerkrys@gmail.com>
2017-07-11 22:46:15 +0000

    Adds jQuery variable naming conventions to composer-char-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,31 +1,31 @@
 $(document).ready(function() {
   //console.log("Document is ready");
-  var textArea = $('.new-tweet').find('textarea');
-  $(textArea).on('focus', function() {
+  var $textArea = $('.new-tweet').find('textarea');
+  $($textArea).on('focus', function() {
     //console.log("Focus is on textArea");
     // DRY out of the two functions below on refactor
     $(this).on('keydown', function() {
       currentCharCount = this.value.length;
       var remainingCharCount = 140 - this.value.length;
-      var counter = $(this).closest('form').find('.counter');
-      $(counter).text(remainingCharCount);
+      var $counter = $(this).closest('form').find('.counter');
+      $($counter).text(remainingCharCount);
       if (remainingCharCount < 0) {
-        counter.addClass('negativeCharCount');
+        $counter.addClass('negativeCharCount');
       }
       if (remainingCharCount >= 0) {
-        counter.removeClass('negativeCharCount');
+        $counter.removeClass('negativeCharCount');
       }
     });
     $(this).on('keyup', function() {
       currentCharCount = this.value.length;
       var remainingCharCount = 140 - this.value.length;
-      var counter = $(this).closest('form').find('.counter');
-      $(counter).text(remainingCharCount);
+      var $counter = $(this).closest('form').find('.counter');
+      $($counter).text(remainingCharCount);
       if (remainingCharCount < 0) {
-        counter.addClass('negativeCharCount');
+        $counter.addClass('negativeCharCount');
       }
       if (remainingCharCount >= 0) {
-        counter.removeClass('negativeCharCount');
+        $counter.removeClass('negativeCharCount');
       }
     });
   });
