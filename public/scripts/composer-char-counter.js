eab68a4bfbaada3978bcfa2e8afc1f3474b2d14c
Tyler Krys <tylerkrys@gmail.com>
2017-07-11 20:46:41 +0000

    Adds jQuery logic to update .counter with remaining character count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
-  console.log("Document is ready");
+  //console.log("Document is ready");
   var textArea = $('.new-tweet').find('textarea');
   $(textArea).on('focus', function() {
-    console.log("Focus is on textArea");
-    $(this).on('keyup', function() {
+    //console.log("Focus is on textArea");
+    $(this).on('keydown', function() {
       currentCharCount = this.value.length;
-      console.log("The character count is " + currentCharCount);
+      //console.log("The character count is " + currentCharCount);
       var remainingCharCount = 140 - this.value.length;
-      console.log("The remaining character count is " + remainingCharCount);
-    })
+      //console.log("The remaining character count is " + remainingCharCount);
+      var counter = $(this).closest('form').find('.counter');
+      $(counter).text(remainingCharCount);
+    });
   });
 });
