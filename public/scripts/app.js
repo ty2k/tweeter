5f11bbdfc440904eec9213cbfdc4ed1c6e58979b
Tyler Krys <tylerkrys@gmail.com>
2017-07-12 22:10:29 +0000

    Adds renderTweets() to call createTweetElement() on each tweet in an array

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,22 +6,53 @@
 
 $(document).ready(function() {
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  var tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+  // Fake data taken from tweets.json. Test / driver code (temporary). Eventually will get this from the server.
+  var data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
       },
-      "handle": "@SirIsaac"
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
     },
-    "created_at": 1461116232227
-  }
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
 
   // createTweetElement() takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet
   function createTweetElement(targetTweet) {
@@ -54,12 +85,17 @@ $(document).ready(function() {
     return $tweet;
   }
 
-  // Call the function using the tweetData table above as a dummy database
-  var $tweet = createTweetElement(tweetData);
+  // renderTweets calls createTweetElement on each element in the tweets array and prepends it to the #tweets-container in index.html
+  function renderTweets(tweets) {
+  // Loop through our tweets database object
+    for (var i = 0; i < data.length; i++) {
+      // Call createTweetElement for each tweet
+      var currentTweet = createTweetElement(data[i]);
+      // Take return value of createTweetElement and appends it to the tweets container
+      $('#tweets-container').prepend(currentTweet);
+    }
+  }
 
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  // Added the ID #tweets-container to our .tweet-stream section
-  $('#tweets-container').prepend($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc. Changed append to prepend to make the tweet appear before the rest.
+  renderTweets(data);
 
 });
