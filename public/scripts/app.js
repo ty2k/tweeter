2410c027ede93de3cb24a0ebebf008e7ab49f8fa
Tyler Krys <tylerkrys@gmail.com>
2017-07-12 19:55:17 +0000

    Adds logic to createTweetElement() to output an HTML tweet given a JSON tweet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -25,9 +25,37 @@ $(document).ready(function() {
 
   // createTweetElement() takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet
   function createTweetElement(targetTweet) {
-      var $tweet = $("<article>").addClass("tweet");
-      // This function shouldn't insert the created DOM structure to the page. It should instead just return the $tweet to the caller
-      return $tweet;
+    // Header - Avatar
+    var avatarURLSmall = targetTweet.user.avatars.small;
+    var $avatar = $("<img>").attr("src", avatarURLSmall).addClass("light-avatar");
+    // Header - Display name
+    var displayNameText = targetTweet.user.name;
+    var $displayName = $("<span>").addClass("light-display-name").append(displayNameText);
+    // Header - Handle
+    var handleText = targetTweet.user.handle;
+    var $handle = $("<span>").addClass("light-handle").append(handleText);
+    // Header - complete, combining avatar, display name, handle
+    var $header = $("<header>").addClass("light-header").append($avatar).append($displayName).append($handle);
+    // Tweet body
+    var $tweetBody = $("<p>").append(targetTweet.content.text);
+    // Footer - span time since post
+    // Need to refactor this to convert from unix time, subtract it from current time
+    var $timeCreatedAt = $("<span>").addClass("time-since-post").append(targetTweet.created_at);
+    console.log($timeCreatedAt);
+    var currentTime = (new Date().getTime());
+    console.log("Current time: " + currentTime);
+    var timeSincePostCreated = currentTime - targetTweet.created_at;
+    console.log("Time since post created: " + timeSincePostCreated);
+    // Footer - interaction icons
+    var iconFlag = $("<i>").addClass("fa fa-flag").attr("aria-hidden", "true");
+    var iconRetweet = $("<i>").addClass("fa fa-retweet").attr("aria-hidden", "true");
+    var iconHeart = $("<i>").addClass("fa fa-heart").attr("aria-hidden", "true");
+    var $interactionIcons = $("<span>").addClass("interaction-icons").append(iconFlag, iconRetweet, iconHeart);
+    // Footer - complete
+    var $footer = $("<footer>").append($timeCreatedAt).append($interactionIcons);
+    var $tweet = $("<article>").addClass("tweet").append($header).append($tweetBody).append($footer);
+    // This function shouldn't insert the created DOM structure to the page. It should instead just return the $tweet to the caller
+    return $tweet;
   }
 
   // Call the function using the tweetData table above as a dummy database
