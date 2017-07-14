6aba13b99669f2f9926d07752e95f62fd47cb031
Tyler Krys <tylerkrys@gmail.com>
2017-07-14 18:00:41 +0000

    Refactor tweet-hover.js into main app.js file so dynamic styling is available after tweets are loaded asynchronously

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,11 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// jQuery is already loaded on index.html, so do everything inside ready()
 
 $(document).ready(function() {
 
-  // Fake data taken from tweets.json. Test / driver code (temporary). Eventually will get this from the server.
+  // Create data array var that can be accessed anywhere in ready() function
   var data = [];
-  var newTweetsData = [];
 
-  // createTweetElement() takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet
+  // createTweetElement() takes a tweet object and return a tweet <article> element containing the entire HTML structure of the tweet
   function createTweetElement(targetTweet) {
     // Header - Avatar
     var avatarURLSmall = targetTweet.user.avatars.small;
@@ -34,28 +29,26 @@ $(document).ready(function() {
     var iconRetweet = $("<i>").addClass("fa fa-retweet").attr("aria-hidden", "true");
     var iconHeart = $("<i>").addClass("fa fa-heart").attr("aria-hidden", "true");
     var $interactionIcons = $("<span>").addClass("interaction-icons").append(iconFlag, iconRetweet, iconHeart);
-    // Footer - complete
+    // Footer - complete, combining time and icons
     var $footer = $("<footer>").append($timeCreatedAt).append($interactionIcons);
     var $tweet = $("<article>").addClass("tweet").append($header).append($tweetBody).append($footer);
-    // This function shouldn't insert the created DOM structure to the page. It should instead just return the $tweet to the caller
+    // Return the $tweet HTML structure to the caller
     return $tweet;
   }
 
   // renderTweets calls createTweetElement on each element in the tweets array and prepends it to the #tweets-container in index.html
   function renderTweets(tweets) {
-    setTimeout(function() {
-      console.log("We're here in the renderTweets function");
-      // Loop through our tweets database object
-      for (var i = 0; i < tweets.length; i++) {
-        // Call createTweetElement for each tweet
-        var currentTweet = createTweetElement(tweets[i]);
-        // Take return value of createTweetElement and appends it to the tweets container
-        $('#tweets-container').prepend(currentTweet);
-      }
-    }, 200);
+    // Loop through our tweets database object
+    for (var i = 0; i < tweets.length; i++) {
+      // Call createTweetElement for each tweet
+      var currentTweet = createTweetElement(tweets[i]);
+      // Take return value of createTweetElement, prepend it to #tweets-container
+      $('#tweets-container').prepend(currentTweet);
+    }
+    hoverOverTweet();
   }
 
-  // Handle incoming tweets as the form on index.html is submitted with a POST to /tweets
+  // A function to handle incoming tweets through a POST route to /tweets
   function handleNewTweet(event) {
     // Use event.preventDefault to stop the browser from leaving the page
     event.preventDefault();
@@ -69,7 +62,6 @@ $(document).ready(function() {
       alert("Your tweet must be 140 characters or less!");
     // Otherwise, make an AJAX POST request to /tweets
     } else {
-      console.log("A new tweet is here!");
       $.ajax({
         type: 'POST',
         url:  '/tweets',
@@ -78,12 +70,16 @@ $(document).ready(function() {
       // Reset textarea input and character count
       $(".new-tweet").find("textarea").val("");
       $(".new-tweet").find("span").text("140");
+      // Empty the HTML from the #tweets-container
       $("#tweets-container").empty();
-      // loadTweets again to grab the new tweet
+      // Empty the data array variable
+      data = [];
+      // Go to loadTweets() to fill the data array variable again
       loadTweets();
     }
   }
   const $form = $(".new-tweet").find("form");
+  // When the .new-tweet form on index.html is submitted, handleNewTweet()
   $form.on('submit', handleNewTweet);
 
   // Fetch tweets through a GET route to /tweets
@@ -92,25 +88,60 @@ $(document).ready(function() {
       url: '/tweets',
       method: 'GET',
       success: function (tweetsArray) {
-        console.log('Success: ', tweetsArray);
-        if (!data[0]) {
-          data = tweetsArray;
-          renderTweets(data);
-        } else {
-          newTweetsData = [];
-          for (var j = 0; j < tweetsArray.length; j++) {
-            if (data.indexOf(tweetsArray[j]) === -1) {
-              data.push(tweetsArray[j]);
-              newTweetsData.push(tweetsArray[j]);
-            }
-          }
-          renderTweets(newTweetsData);
-        }
-        //console.log(data);
+        // Fill the data array variable with all the tweets
+        data = tweetsArray;
+        renderTweets(data);
       }
     });
   }
-  // Immediately call loadTweets() after declaring it, we need them immediately on load
+  // Call loadTweets() after declaring it, we need it immediately on first load
   loadTweets();
+
+  function hoverOverTweet() {
+    // When mouse-overing a tweet
+    $(".tweet").on("mouseover", function(event) {
+      console.log("Mouseover on a .tweet");
+      event.stopPropagation();
+      // Darken the border
+      $(this).addClass("darker-border");
+      // Show the interaction icons
+      var $icons = $(this).find(".interaction-icons");
+      $icons.addClass("visible-icons").removeClass("interaction-icons");
+      // Darken the header background
+      var $header = $(this).find("header");
+      $header.addClass("darker-header").removeClass("light-header");
+      // Darken the avatar
+      var $avatar = $(this).find(".light-avatar");
+      $avatar.addClass("darker-avatar").removeClass("light-avatar");
+      // Darken the display name
+      var $displayName = $(this).find(".light-display-name");
+      $displayName.addClass("darker-display-name").removeClass("light-display-name");
+      // Darken the handle
+      var $handle = $(this).find(".light-handle");
+      $handle.addClass("darker-handle").removeClass("light-handle");
+    });
+    // When mouse-leaving a tweet
+    $(".tweet").on("mouseleave", function(event) {
+      console.log("Mouseleave from a .tweet");
+      event.stopPropagation();
+      // Lighten the border
+      $(this).removeClass("darker-border");
+      // Hide the interaction icons
+      var $icons = $(this).find(".visible-icons");
+      $icons.addClass("interaction-icons").removeClass("visible-icons");
+      // Lighten the header background
+      var $header = $(this).find("header");
+      $header.addClass("light-header").removeClass("darker-header");
+      // Lighten the avatar
+      var $avatar = $(this).find(".darker-avatar");
+      $avatar.addClass("light-avatar").removeClass("darker-avatar");
+      // Lighten the display name
+      var $displayName = $(this).find(".darker-display-name");
+      $displayName.addClass("light-display-name").removeClass("darker-display-name");
+      // Lighten the handle
+      var $handle = $(this).find(".darker-handle");
+      $handle.addClass("light-handle").removeClass("darker-handle");
+    });
+  }
 
 });
