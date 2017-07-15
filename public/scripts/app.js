31be22b79c5093bc4ac8116ccdce287ec08aa7ed
Tyler Krys <tylerkrys@gmail.com>
2017-07-15 06:40:03 +0000

    Linting, re-arranges order of functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,104 +1,8 @@
-// jQuery is loaded on index.html, so do everything inside ready()
-
+// jQuery is loaded on index.html, do run the app on ready()
 $(document).ready(function() {
 
   // Create data array var that can be accessed anywhere in ready() function
   var data = [];
-
-  // Take a tweet object and return a tweet <article> element containing the entire HTML structure of the tweet
-  function createTweetElement(targetTweet) {
-    // Header - Avatar
-    var avatarURLSmall = targetTweet.user.avatars.small;
-    var $avatar = $("<img>").attr("src", avatarURLSmall).addClass("light-avatar");
-    // Header - Display name
-    var displayNameText = targetTweet.user.name;
-    var $displayName = $("<span>").addClass("light-display-name").append(displayNameText);
-    // Header - Handle
-    var handleText = targetTweet.user.handle;
-    var $handle = $("<span>").addClass("light-handle").append(handleText);
-    // Header - complete, combining avatar, display name, handle
-    var $header = $("<header>").addClass("light-header").append($avatar).append($displayName).append($handle);
-    // Tweet body, escaped
-    var $tweetBody = $("<p>").text(targetTweet.content.text);
-    // Footer - span time since post
-    // Use Moment.js with created_at value to produce a pretty string to output
-    var timeSincePostCreated = moment.unix(targetTweet.created_at/1000).fromNow();
-    var timeAndDatePostCreated = new Date(targetTweet.created_at);
-    var $timeCreatedAt = $("<span>").addClass("time-since-post").append(timeSincePostCreated).attr("title", timeAndDatePostCreated);
-    // Footer - interaction icons
-    var iconFlag = $("<i>").addClass("fa fa-flag").attr("aria-hidden", "true").attr("title", "Flag tweet");
-    var iconRetweet = $("<i>").addClass("fa fa-retweet").attr("aria-hidden", "true").attr("title", "Retweet");
-    var iconHeart = $("<i>").addClass("fa fa-heart").attr("aria-hidden", "true").attr("title", "Heart tweet");
-    var $interactionIcons = $("<span>").addClass("interaction-icons").append(iconHeart, iconRetweet, iconFlag);
-    // Footer - complete, combining time and icons
-    var $footer = $("<footer>").append($timeCreatedAt).append($interactionIcons);
-    // Tweet object - complete, combining header, tweet body, and footer
-    var $tweet = $("<article>").addClass("tweet").append($header).append($tweetBody).append($footer);
-    // Return the $tweet HTML structure
-    return $tweet;
-  }
-
-  // Calls createTweetElement on each element in the tweets array and prepends it to the #tweets-container in index.html
-  function renderTweets(tweets) {
-    // Loop through our tweets database object
-    for (var i = 0; i < tweets.length; i++) {
-      // Call createTweetElement for each tweet
-      var currentTweet = createTweetElement(tweets[i]);
-      // Take HTML return value, prepend it to #tweets-container
-      $("#tweets-container").prepend(currentTweet);
-    }
-    // After tweets are rendered, hoverOverTweet() uses jQuery to style them dynamically
-    hoverOverTweet();
-  }
-
-  // Handle incoming tweets through a POST route to /tweets
-  function handleNewTweet(event) {
-    // Use event.preventDefault to stop the browser from leaving the page
-    event.preventDefault();
-    // Grab our tweet body text to conditionally inspect the length
-    var $textAreaInput = $(".new-tweet").find("textarea").val();
-    // If the tweet is empty, reject it and inform the user
-    if ($textAreaInput.length === 0) {
-      $.flash("You need to write a tweet before tweeting!");
-    // If the tweet is longer than 140 characters, reject it and inform the user
-    } else if ($textAreaInput.length > 140) {
-      $.flash("Your tweet must be 140 characters or less!");
-    // Otherwise, make an AJAX POST request to /tweets
-    } else {
-      $.ajax({
-        type: "POST",
-        url:  "/tweets",
-        data: $(this).serialize()
-      })
-      // Reset textarea input and character count
-      $(".new-tweet").find("textarea").val("");
-      $(".new-tweet").find("span").text("140");
-      // Empty the HTML from the #tweets-container
-      $("#tweets-container").empty();
-      // Empty the data array variable
-      data = [];
-      // Go to loadTweets() to fill the data array variable with latest tweet data
-      loadTweets();
-    }
-  }
-  const $form = $(".new-tweet").find("form");
-  // When the .new-tweet form on index.html is submitted, handleNewTweet()
-  $form.on("submit", handleNewTweet);
-
-  // Fetch tweets through a GET route to /tweets
-  function loadTweets(event) {
-    $.ajax({
-      url: "/tweets",
-      method: "GET",
-      success: function (tweetsArray) {
-        // Fill the data array variable with all the tweets
-        data = tweetsArray;
-        renderTweets(data);
-      }
-    });
-  }
-  // Call loadTweets() after declaring it, we need it immediately on page load
-  loadTweets();
 
   // Styles .tweet elements dynamically on mouseover and mouseleave
   function hoverOverTweet() {
@@ -145,5 +49,102 @@ $(document).ready(function() {
       $handle.addClass("light-handle").removeClass("darker-handle");
     });
   }
+
+  // Take a tweet object and return a tweet <article> element containing the entire HTML structure of the tweet
+  function createTweetElement(targetTweet) {
+    // Header - Avatar
+    var avatarURLSmall = targetTweet.user.avatars.small;
+    var $avatar = $("<img>").attr("src", avatarURLSmall).addClass("light-avatar");
+    // Header - Display name
+    var displayNameText = targetTweet.user.name;
+    var $displayName = $("<span>").addClass("light-display-name").append(displayNameText);
+    // Header - Handle
+    var handleText = targetTweet.user.handle;
+    var $handle = $("<span>").addClass("light-handle").append(handleText);
+    // Header - complete, combining avatar, display name, handle
+    var $header = $("<header>").addClass("light-header").append($avatar).append($displayName).append($handle);
+    // Tweet body, escaped
+    var $tweetBody = $("<p>").text(targetTweet.content.text);
+    // Footer - span time since post
+    // Use Moment.js with created_at value to produce a pretty string to output
+    var timeSincePostCreated = moment.unix(targetTweet.created_at / 1000).fromNow();
+    var timeAndDatePostCreated = new Date(targetTweet.created_at);
+    var $timeCreatedAt = $("<span>").addClass("time-since-post").append(timeSincePostCreated).attr("title", timeAndDatePostCreated);
+    // Footer - interaction icons
+    var iconFlag = $("<i>").addClass("fa fa-flag").attr("aria-hidden", "true").attr("title", "Flag tweet");
+    var iconRetweet = $("<i>").addClass("fa fa-retweet").attr("aria-hidden", "true").attr("title", "Retweet");
+    var iconHeart = $("<i>").addClass("fa fa-heart").attr("aria-hidden", "true").attr("title", "Heart tweet");
+    var $interactionIcons = $("<span>").addClass("interaction-icons").append(iconHeart, iconRetweet, iconFlag);
+    // Footer - complete, combining time and icons
+    var $footer = $("<footer>").append($timeCreatedAt).append($interactionIcons);
+    // Tweet object - complete, combining header, tweet body, and footer
+    var $tweet = $("<article>").addClass("tweet").append($header).append($tweetBody).append($footer);
+    // Return the $tweet HTML structure
+    return $tweet;
+  }
+
+  // Calls createTweetElement on each element in the tweets array and prepends it to the #tweets-container in index.html
+  function renderTweets(tweets) {
+    // Loop through our tweets database object
+    for (var i = 0; i < tweets.length; i++) {
+      // Call createTweetElement for each tweet
+      var currentTweet = createTweetElement(tweets[i]);
+      // Take HTML return value, prepend it to #tweets-container
+      $("#tweets-container").prepend(currentTweet);
+    }
+    // After tweets are rendered, hoverOverTweet() uses jQuery to style them dynamically
+    hoverOverTweet();
+  }
+
+  // Fetch tweets through a GET route to /tweets
+  function loadTweets(event) {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      success: function (tweetsArray) {
+        // Fill the data array variable with all the tweets
+        data = tweetsArray;
+        renderTweets(data);
+      }
+    });
+  }
+
+  // Call loadTweets() after declaring it, we need it immediately on page load
+  loadTweets();
+
+  // Handle incoming tweets through a POST route to /tweets
+  function handleNewTweet(event) {
+    // Use event.preventDefault to stop the browser from leaving the page
+    event.preventDefault();
+    // Grab our tweet body text to conditionally inspect the length
+    var $textAreaInput = $(".new-tweet").find("textarea").val();
+    // If the tweet is empty, reject it and inform the user
+    if ($textAreaInput.length === 0) {
+      $.flash("You need to write a tweet before tweeting!");
+    // If the tweet is longer than 140 characters, reject it and inform the user
+    } else if ($textAreaInput.length > 140) {
+      $.flash("Your tweet must be 140 characters or less!");
+    // Otherwise, make an AJAX POST request to /tweets
+    } else {
+      $.ajax({
+        type: "POST",
+        url: "/tweets",
+        data: $(this).serialize()
+      });
+      // Reset textarea input and character count
+      $(".new-tweet").find("textarea").val("");
+      $(".new-tweet").find("span").text("140");
+      // Empty the HTML from the #tweets-container
+      $("#tweets-container").empty();
+      // Empty the data array variable
+      data = [];
+      // Go to loadTweets() to fill the data array variable with latest tweet data
+      loadTweets();
+    }
+  }
+
+  // Find the .new-tweet submission form, and handleNewTweet() on submit
+  const $form = $(".new-tweet").find("form");
+  $form.on("submit", handleNewTweet);
 
 });
